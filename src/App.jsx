1a662744ebaddd996e1696e64d320107d7b52fff
Exequiel <e.x.e_salinas3@outlook.com>
2022-09-01 16:49:41 -0300

    Instalando redux

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Header from './components/Header'
+import Productos from './components/Productos'
+import NuevoProducto from './components/NuevoProducto'
+import EditarProducto from './components/EditarProducto'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Header/>
+      <div className='container mt-5'>
+          <Routes>
+            <Route path="/" element={<Productos/>}/>
+            <Route path="/productos/nuevo" element={<NuevoProducto/>}/>
+            <Route path="/productos/editar/:id" element={<EditarProducto/>}/>
+          </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
